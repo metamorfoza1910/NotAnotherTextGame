2360c4cee176a141b5aa127b5fad2a20fe483efd
superhero12 <gorgonkuba@gmail.com>
2021-02-21 17:10:30 +0100

    Added Text Mesh Pro, fixed bugs

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -34,19 +34,14 @@ public class AdventureGame : MonoBehaviour
     {
         var nextStates = state.GetNextStates();
 
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            state = nextStates[0];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            state = nextStates[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
-        }
 
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
 
+        }
     }
 }
